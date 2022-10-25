6574ecbf76a003396ae970ff7d842edefbef40f5
NanorGosdanian <nanorgosdanian@gmail.com>
2022-10-25 20:45:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+const drivers = ['Ali', 'Sammy', 'Suzy', 'Ani', 'Smith', 'bobby']
+
+function findMatching(drivers, name){
+let filteredDrivers = drivers.filter(driver => {
+    if (driver.toLowerCase() === name.toLowerCase() )
+    return true
+    else return false
+}) 
+return filteredDrivers
+}
+
+function fuzzyMatch(drivers, fuzzyname){
+    let filteredDrivers = drivers.filter(driver => {
+        if (driver.startsWith(fuzzyname))
+        return true
+        else return false
+    }) 
+    return filteredDrivers
+}
+function matchName(drivers, name){
+    let filteredDrivers = drivers.filter(driver => {
+        if (driver.name === name )
+        return true
+        else return false
+    }) 
+    return filteredDrivers
+    }
+
+
+
+   
